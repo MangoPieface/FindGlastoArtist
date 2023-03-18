63847be2d79c7638a1ba705c7808903e20474e44
Steve Newstead <steve.newstead@gmail.com>
2023-03-18 20:33:29 +0000

    all my artist changes

---
+++ lib/artists.ts
@@ -0,0 +1,261 @@
+const artists = [
+      {
+        "artist": "Sir Paul McCartney",
+        "town": "Liverpool",
+        "country": "England",
+        "latitude": 53.4084,
+        "longitude": -2.9916,
+        "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+      },
+      {
+        "artist": "Dua Lipa",
+        "town": "London",
+        "country": "England",
+        "latitude": 51.5074,
+        "longitude": -0.1278,
+        "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+      },
+      {
+        "artist": "Pet Shop Boys",
+        "town": "London",
+        "country": "England",
+        "latitude": 51.5074,
+        "longitude": -0.1278,
+        "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+      },
+      {
+        "artist": "Fatboy Slim",
+        "town": "Bromley",
+        "country": "England",
+        "latitude": 51.406,
+        "longitude": 0.0132,
+        "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+      },
+      {
+        "artist": "Thom Yorke",
+        "town": "Wellingborough",
+        "country": "England",
+        "latitude": 52.3023,
+        "longitude": -0.6946,
+        "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+      },
+      {
+        "artist": "Kano",
+        "town": "London",
+        "country": "England",
+        "latitude": 51.5074,
+        "longitude": -0.1278,
+        "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+      },
+      {
+        "artist": "The Charlatans",
+        "town": "West Midlands",
+        "country": "England",
+        "latitude": 52.4751,
+        "longitude": -1.8298,
+        "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+      },
+      {
+        "artist": "Elbow",
+        "town": "Bury",
+        "country": "England",
+        "latitude": 53.5933,
+        "longitude": -2.2966,
+        "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+      },
+      {
+        "artist": "Skunk Anansie",
+        "town": "London",
+        "country": "England",
+        "latitude": 51.5074,
+        "longitude": -0.1278,
+        "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+      },
+      {
+        "artist": "Happy Mondays",
+        "town": "Salford",
+        "country": "England",
+        "latitude": 53.4875,
+        "longitude": -2.2904,
+        "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+      },
+      {
+          "artist": "The Streets",
+          "town": "Birmingham",
+          "country": "England",
+          "latitude": 52.4862,
+          "longitude": -1.8904,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Supergrass",
+          "town": "Oxford",
+          "country": "England",
+          "latitude": 51.7520,
+          "longitude": -1.2577,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Caribou",
+          "town": "Dundas",
+          "country": "England",
+          "latitude": 43.2668,
+          "longitude": -79.9569,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Manic Street Preachers",
+          "town": "Blackwood",
+          "country": "Wales",
+          "latitude": 51.6659,
+          "longitude": -3.1938,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Goldfrapp",
+          "town": "London",
+          "country": "England",
+          "latitude": 51.5074,
+          "longitude": -0.1278,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Metronomy",
+          "town": "Totnes",
+          "country": "England",
+          "latitude": 50.4302,
+          "longitude": -3.6842,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Sleaford Mods",
+          "town": "Nottingham",
+          "country": "England",
+          "latitude": 52.9548,
+          "longitude": -1.1581,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Mabel",
+          "town": "London",
+          "country": "England",
+          "latitude": 51.5074,
+          "longitude": -0.1278,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Bicep",
+          "town": "Belfast",
+          "country": "Northern Ireland",
+          "latitude": 54.5973,
+          "longitude": -5.9301,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Baxter Dury",
+          "town": "London",
+          "country": "England",
+          "latitude": 51.5074,
+          "longitude": -0.1278,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Beabadoobee",
+          "town": "London",
+          "country": "England",
+          "latitude": 51.5074,
+          "longitude": -0.1278,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "The Big Moon",
+          "town": "London",
+          "country": "England",
+          "latitude": 51.5074,
+          "longitude": -0.1278,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Squid",
+          "town": "Brighton",
+          "country": "England",
+          "latitude": 50.8225,
+          "longitude": -0.1372,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Jade Bird",
+          "town": "Hexham",
+          "country": "England",
+          "latitude": 54.9714,
+          "longitude": -2.1030,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Black Country, New Road",
+          "town": "London",
+          "country": "England",
+          "latitude": 51.5074,
+          "longitude": -0.1278,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Anna Calvi",
+          "town": "London",
+          "country": "England",
+          "latitude": 51.5074,
+          "longitude": -0.1278,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Nadine Shah",
+          "town": "Whitburn",
+          "country": "England",
+          "latitude": 54.9618,
+          "longitude": -1.4410,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Dry Cleaning",
+          "town": "London",
+          "country": "England",
+          "latitude": 51.5074,
+          "longitude": -0.1278,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "EOB",
+          "town": "Oxford",
+          "country": "England",
+          "latitude": 51.7520,
+          "longitude": -1.2577,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Laura Marling",
+          "town": "Eversley",
+          "country": "England",
+          "latitude": 51.3256,
+          "longitude": -0.8752,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Working Men's Club",
+          "town": "Todmorden",
+          "country": "England",
+          "latitude": 53.7163,
+          "longitude": -2.0988,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        },
+        {
+          "artist": "Arlo Parks",
+          "town": "London",
+          "country": "England",
+          "latitude": 51.5074,
+          "longitude": -0.1278,
+          "spotify": "https://open.spotify.com/artist/PLACEHOLDER"
+        }
+  ];
+  
+  export default artists;
+  
---
+++ pages/api/closest-band.ts
@@ -0,0 +1,47 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import artists from "../../lib/artists";
+import * as geolib from "geolib";
+
+
+type Data = {
+  artist: string;
+  town: string;
+  country: string;
+  latitude: number;
+  longitude: number;
+  spotify: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    const userLocation = {
+     // latitude: parseFloat(req.query.lat as string),
+     // longitude: parseFloat(req.query.lng as string),
+     latitude: parseFloat("55.8642" as string),
+     longitude: parseFloat("4.2518" as string),
+    };
+
+    // Find the closest band
+    const distances = artists.map(artist => ({
+        index: artists.indexOf(artist),
+        distance: geolib.getDistance(userLocation, {
+          latitude: artist.latitude,
+          longitude: artist.longitude,
+        }),
+      }));
+      
+      distances.sort((a, b) => a.distance - b.distance);
+      const closestBand = artists[distances[0].index];
+      
+      
+
+      res.status(200).json(closestBand);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred while processing your request." });
+  }
+}
